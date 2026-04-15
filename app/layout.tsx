bfa3a8b8155ec4aa69d19fc6dfd872213ce80613
doninidjessa <doninidjessa@gmail.com>
2026-04-15 10:52:18 +0000

    adding go to top button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import ScrollToTop from "../components/ScrollToTop";
 import { LanguageProvider } from "../context/LanguageContext";
 
 const geistSans = Geist({
@@ -53,6 +54,7 @@ export default function RootLayout({
           <div className="global-grid-mask"></div>
           <Navbar />
           {children}
+          <ScrollToTop />
         </LanguageProvider>
       </body>
     </html>
